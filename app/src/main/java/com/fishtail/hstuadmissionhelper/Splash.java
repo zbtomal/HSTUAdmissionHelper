1407de4baeb4075a9987a0f22501fcb3e5d1add4
Zikrul Bari Tomal <14892096+zbtomal@users.noreply.github.com>
2025-01-22 19:19:15 +0600

    fixed the bug on the splash activity

--- app/src/main/java/com/fishtail/hstuadmissionhelper/Splash.java
+++ app/src/main/java/com/fishtail/hstuadmissionhelper/Splash.java
@@ -22,6 +22,7 @@ public class Splash extends AppCompatActivity {
             @Override
             public void run() {
                 Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
                 finish();
             }
         }, 3000);
