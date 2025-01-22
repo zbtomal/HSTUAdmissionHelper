8a921643a436174beac8b4e07618431d7e3bb9c6
jannat-shanto <shanto2102022@gmail.com>
2025-01-22 15:46:17 +0600

    I've Designed the MainActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.fishtail.hstuadmissionhelper"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.fishtail.hstuadmissionhelper"
