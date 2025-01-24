0e75efeedf40ef1dd3ca6b5d484f2633410938ca
Zikrul Bari Tomal <14892096+zbtomal@users.noreply.github.com>
2025-01-24 12:49:35 +0600

    Made txt_room_info dynamic

--- app/src/main/java/com/fishtail/hstuadmissionhelper/RoomFinder.java
+++ app/src/main/java/com/fishtail/hstuadmissionhelper/RoomFinder.java
@@ -1,6 +1,10 @@
 package com.fishtail.hstuadmissionhelper;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,36 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class RoomFinder extends AppCompatActivity {
 
+    EditText edt_roll;
+    TextView txt_room_info;
+    Button btn_submit_roll;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_room_finder);
+
+
+
+        edt_roll = findViewById(R.id.edt_roll);
+        txt_room_info = findViewById(R.id.txt_room_info);
+        btn_submit_roll = findViewById(R.id.btn_submit_roll);
+
+        // set the visibilty of the textview to GONE
+        txt_room_info.setVisibility(View.GONE);
+
+        btn_submit_roll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // get the roll number from the edit text
+                String roll = edt_roll.getText().toString();
+
+                // make the textview visible
+                txt_room_info.setVisibility(View.VISIBLE);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
