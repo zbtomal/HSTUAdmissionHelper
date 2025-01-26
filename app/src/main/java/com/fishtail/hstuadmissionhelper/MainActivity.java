6fd48d757acf6d39045f432f95d7c14491d90ef3
Zikrul Bari Tomal <14892096+zbtomal@users.noreply.github.com>
2025-01-26 15:44:05 +0600

    Initialized the buttons for cardview

--- app/src/main/java/com/fishtail/hstuadmissionhelper/MainActivity.java
+++ app/src/main/java/com/fishtail/hstuadmissionhelper/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -19,11 +20,21 @@ public class MainActivity extends AppCompatActivity {
     //ImageButton btn_important;
     //Button btn_notice;
 
+    CardView roomCard;
+    CardView transportCard;
+    CardView importantDatesCard;
+    CardView noticeCard;
+    CardView resultCard;
+    CardView moreCard;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+
+
 
       /*  // Initialize the buttons
         btn_room = findViewById(R.id.imgbtn_room);
@@ -63,6 +74,47 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         }); */
+
+        roomCard = findViewById(R.id.roomCard);
+        transportCard = findViewById(R.id.transportCard);
+        importantDatesCard = findViewById(R.id.importantDatesCard);
+        noticeCard = findViewById(R.id.noticeCard);
+        resultCard = findViewById(R.id.resultCard);
+        moreCard = findViewById(R.id.moreCard);
+
+        roomCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RoomFinder.class);
+                startActivity(intent);
+            }
+        });
+
+        transportCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TransportActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        importantDatesCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ImportantDatesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        noticeCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NoticeBoardActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
