6fc3f1596b46b05b76ed5b007a1616302967f12d
Zikrul Bari Tomal <14892096+zbtomal@users.noreply.github.com>
2025-02-06 12:52:13 +0600

    Some color changes

--- app/src/main/java/com/fishtail/hstuadmissionhelper/NoticeBoardActivity.java
+++ app/src/main/java/com/fishtail/hstuadmissionhelper/NoticeBoardActivity.java
@@ -15,6 +15,7 @@ public class NoticeBoardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_notice_board);
 
