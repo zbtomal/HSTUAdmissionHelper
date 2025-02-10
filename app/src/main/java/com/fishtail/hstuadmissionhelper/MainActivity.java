2b57dff35afcc4ae3d9fec4ab85eeee7173165e4
jannat-shanto <shanto2102022@gmail.com>
2025-02-10 23:28:50 +0600

    Designed more activity

--- app/src/main/java/com/fishtail/hstuadmissionhelper/MainActivity.java
+++ app/src/main/java/com/fishtail/hstuadmissionhelper/MainActivity.java
@@ -88,6 +88,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        moreCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, More.class);
+                startActivity(intent);
+            }
+        });
+
 
         // Image Slider
         imageSlider=findViewById(R.id.imageSlider);
---
+++ app/src/main/java/com/fishtail/hstuadmissionhelper/More.java
@@ -0,0 +1,36 @@
+package com.fishtail.hstuadmissionhelper;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class More extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more);
+
+        // Initialize buttons
+
+
+
+
+
+
+        // Adjust for system bars (e.g., status and navigation bars)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
